623ddad28113d89bfb6dc9d84327532ae297d6f4
Andrew Kalil Barrense Magalhães <kalilmagal@gmail.com>
2021-06-30 09:29:48 -0300

    update prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    semi: false,
-    singleQuote: true,
-    arrowParens: 'avoid',
-    trailingComma: 'none',
-    endOnLine: 'auto'
-};
+  semi: false,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  trailingComma: 'none',
+  endOnLine: 'auto'
+}
